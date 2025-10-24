a63870803c3a6ad7b0768d5f1b3b47829e5a020d
nelexey <codenamed.asyncz@gmail.com>
2025-10-24 09:14:29 +0300

    Методы получения записей по id, all по пагинации.

--- database/methods/RC_SKU.py
+++ database/methods/RC_SKU.py
@@ -1,8 +1,9 @@
-from typing import List, Dict, Any
+from typing import List, Dict, Any, Optional, Sequence
 import logging
 
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.engine import Result, ScalarResult
 
 from database.models.RC_SKU import RC_SKU
 
@@ -35,3 +36,35 @@ async def create_sku_metrics(session: AsyncSession, metrics_list: List[Dict[str,
         logger.debug(f"Successfully added {len(metrics_to_add)} new SKU metrics")
     else:
         logger.debug("No new SKU metrics to add")
+
+async def get_all_sku_metrics(session: AsyncSession,
+                            page: int = 1,
+                            per_page: int = 20,) -> Sequence[RC_SKU]:
+    if page < 1:
+        page = 1
+    offset = (page - 1) * per_page
+
+    logger.debug(
+        f"Attempting to fetch SKU metrics: page={page}, per_page={per_page}, offset={offset}"
+    )
+
+    stmt = select(RC_SKU).limit(per_page).offset(offset)
+    result: Result = await session.execute(stmt)
+    metrics_page: Sequence[RC_SKU] = result.scalars().all()
+
+    logger.debug(f"Successfully fetched {len(metrics_page)} SKU metrics for page {page}")
+
+    return metrics_page
+
+
+
+async def get_sku_metrics_by_id(session: AsyncSession, id: int) -> Optional[RC_SKU]:
+    logger.debug(f"Attempting to fetch SKU metric with ID: {id}")
+    
+    stmt = select(RC_SKU).where(RC_SKU.id == id)
+    
+    result: Result = await session.execute(stmt)
+    
+    sku_metric: Optional[RC_SKU] = result.scalars().first()
+        
+    return sku_metric
